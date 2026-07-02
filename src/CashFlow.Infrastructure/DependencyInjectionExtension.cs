00a6017c4aff52f82d779d3e33ff837b69660073
IgoorToorres <igorevangelista121@gmail.com>
2026-07-02 10:58:23 -0300

    refactor - Ajusta mapeamento e organização da infraestrutura

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -11,13 +11,22 @@ public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        AddDbContext(services, configuration);
+        AddRepositories(services);
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IExpensesRepository, ExpensesRepository>();
+    }
+
+    private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
         services.AddDbContext<CashFlowDbContext>(options =>
         {
             options.UseNpgsql(connectionString);
         });
-
-        services.AddScoped<IExpensesRepository, ExpensesRepository>();
     }
 }
