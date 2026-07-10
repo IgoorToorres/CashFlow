d985931fd4cc3f3ed7c0f93dba500ec54c848001
IgoorToorres <igorevangelista121@gmail.com>
2026-07-10 11:31:53 -0300

    feat: adicionar geração de relatório de despesas em excel

--- src/CashFlow.API/controllers/ReportController.cs
+++ src/CashFlow.API/controllers/ReportController.cs
@@ -1,4 +1,5 @@
 using System.Net.Mime;
+using CashFlow.Application.UseCases.Expenses.Reports.Excel;
 using CashFlow.Communication.Requests;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,10 +13,11 @@ public class ReportController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     public async Task<IActionResult> GetExcel(
+        [FromServices] IGenerateExpensesReportExcelUseCase useCase,
         [FromHeader] DateOnly month
     )
     {
-        byte[] file = new byte[1];
+        byte[] file = await useCase.Execute(month);
 
         if(file.Length > 0)
             return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -0,0 +1,44 @@
+using ClosedXML.Excel;
+using CashFlow.Domain.Resources;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
+
+public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
+{
+    public Task<byte[]> Execute(DateOnly month)
+    {
+        var workbook = new XLWorkbook
+        {
+            Author = "Igor"
+        };
+        workbook.Style.Font.FontSize = 12;
+        workbook.Style.Font.FontName = "Times New Roman";
+
+        var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+
+        InsertHeader(worksheet);
+
+        using var stream = new MemoryStream();
+        workbook.SaveAs(stream);
+
+        return Task.FromResult(stream.ToArray());
+    }
+
+    private static void InsertHeader(IXLWorksheet worksheet)
+    {
+        worksheet.Cell("A1").Value = ResourceReportGenerationMessages.TITLE;
+        worksheet.Cell("B1").Value = ResourceReportGenerationMessages.DATE;
+        worksheet.Cell("C1").Value = ResourceReportGenerationMessages.PAYMENT_TYPE;
+        worksheet.Cell("D1").Value = ResourceReportGenerationMessages.AMOUNT;
+        worksheet.Cell("E1").Value = ResourceReportGenerationMessages.DESCRIPTION;
+
+        worksheet.Cells("A1:E1").Style.Font.Bold = true;
+        worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#f5c2b6");
+
+        worksheet.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("C1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+    }
+}
