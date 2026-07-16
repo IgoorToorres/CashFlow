31d5ad5b57343100c03ef5be6c70a51a81166c97
IgoorToorres <igorevangelista121@gmail.com>
2026-07-16 14:11:20 -0300

    feat(usuarios): criptografa senha no cadastro

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,19 +1,24 @@
 using AutoMapper;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Exception.ExceptionBase;
 
 namespace CashFlow.Application.UseCases.Users.Register;
 
-public class RegisterUserUseCase(IMapper mapper) : IRegisterUserUseCase
+public class RegisterUserUseCase(IMapper mapper, IPasswordEncripter passwordEncripter) : IRegisterUserUseCase
 {
     private readonly IMapper _mapper = mapper;
+    private readonly IPasswordEncripter _passwordEncripter = passwordEncripter;
 
     public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
     {
         Validate(request);
 
         var user = _mapper.Map<Domain.Entities.User>(request);
+
+        user.Password = _passwordEncripter.Encrypt(request.Password);
+
         return new ResponseRegisteredUserJson
         {
             Name = user.Name,
